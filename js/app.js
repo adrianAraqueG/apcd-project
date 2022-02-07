d29159fac7be73bc5ef4468ae2306ffec2dbffa7
adrianAraqueG <adrian.kanox.2003@gmail.com>
2022-02-07 11:38:51 -0500

    bugs2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,2 @@
-
-<<<<<<< HEAD
 import { datos as datosGenerales } from './generales.js';
 
-=======
-import {datos as datosGenerales} from './generales.js';
-
-console.log(datosGenerales);
->>>>>>> edb3bd4f4c2440ed14212e816cc19643f0331587
