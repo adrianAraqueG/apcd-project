694b054f8129f7d1b33b7111319569c25427085d
unknown <adrian.kanox.2003@gmail.com>
2022-02-25 13:02:00 -0500

    boton borrarTodo implementado

--- js/app.js
+++ js/app.js
@@ -2,4 +2,13 @@ import { datos, datos as datosGenerales } from './generales.js';
 import { ordenes as ordenes } from './ordenes.js';
 import { integrantes as integrantes } from './integrantes.js';
 
+const btnBorrarTodo = document.querySelector('#borrarTodo');
+
+btnBorrarTodo.addEventListener('click', e => {
+    if(confirm('¿Está seguro de que quiere borrar toda la planilla?')){
+        window.localStorage.clear();
+        location.reload();
+    }
+});
+
 
